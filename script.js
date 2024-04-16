df31d1ceedfac0363f07ae394e473d2bf5dc6e49
Tiago Abudi <tiagoabudi96@gmail.com>
2024-04-16 14:55:14 -0300

    feat: tabs e mapa
    - alterando o componente de mapa
    
    - ajustando as tabs para funcionarem melhor

--- script.js
+++ script.js
@@ -3,3 +3,39 @@ window.addEventListener("scroll", function () {
   let header = document.querySelector("#tab");
   header.classList.toggle("rolagem", this.window.scrollY > 0);
 });
+
+let links = document.querySelectorAll('.js-link');
+let sections = document.querySelectorAll('.sections-products')
+
+window.addEventListener('scroll', () => {
+  sections.forEach(section => {
+    let top = window.scrollY;
+    let offset = section.offsetTop - 40;
+    let heightSection = section.offsetHeight;
+    let idSection = section.getAttribute('id');
+
+    if (top >= offset && top < offset + heightSection) {
+      links.forEach(link => {
+        link.classList.remove('actived')
+
+        document.querySelector(`header section a[href*='${idSection}']`).classList.add('actived');
+      })
+    }
+  })
+})
+function scrollSection(event) {
+  event.preventDefault()
+
+  const href = event.currentTarget.getAttribute('href');
+  const section = document.querySelector(href)
+
+  const topSection = section.offsetTop - 40;
+
+  window.scrollTo({
+    top: topSection,
+    behavior: "smooth"
+  })
+}
+links.forEach(link => {
+  link.addEventListener('click', scrollSection)
+})
